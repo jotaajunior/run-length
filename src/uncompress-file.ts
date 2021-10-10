9bfed64c988a559ca41ac8c4ff1d22c588e1c8b3
Jota <jjunior@protonmail.ch>
2021-10-10 18:17:10 -0300

    feat: add colors to logs

--- src/uncompress-file.ts
+++ src/uncompress-file.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk'
 import fs from 'fs'
 import path from 'path'
 
@@ -8,14 +9,19 @@ const [filePath] = process.argv.slice(2)
 if (fs.existsSync(filePath)) {
   const fileContent = fs.readFileSync(path.resolve(filePath), 'utf-8')
 
-  console.log('Texto descomprimido:', uncompress(fileContent))
+  console.log(
+    [
+      chalk.green.bold('✓ Texto descomprimido com sucesso:'),
+      uncompress(fileContent),
+    ].join('\n'),
+  )
 
   process.exit(0)
 }
 
 console.error(
   [
-    `Arquivo em ${filePath} não existe ou não pode ser lido.`,
+    chalk.red.bold(`Arquivo em '${filePath}' não existe ou não pode ser lido.`),
     'Verifique se o caminho está correto',
   ].join('\n'),
 )
